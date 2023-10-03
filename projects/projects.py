638e9d8a02e040aa537af49691e951fd105b322c
Ashwinv93 <ashwinkrv@gmail.com>
2023-10-03 22:38:25 +0000

    Test 2

--- projects/projects.py
+++ projects/projects.py
@@ -21,3 +21,7 @@ def walruses():
 @app_projects.route('/hawkers/')
 def hawkers():
     return render_template("hawkers.html")
+
+@app_projects.route('/Test/')
+def Test():
+    return render_template("Test.html")

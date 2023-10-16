faf2ace2d5f4e5ef0077098097f627e61a23a1c0
RonitT1234 <ronit.joe@gmail.com>
2023-10-16 07:36:41 -0700

    Resetting for API Attempt #2

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from __init__ import app,db  # Definitions initialization
 from model.jokes import initJokes
 from model.users import initUsers
 from model.players import initPlayers
-from model.pokemons import initPokemon
 
 
 # setup APIs
@@ -16,7 +15,6 @@ from api.covid import covid_api # Blueprint import api definition
 from api.joke import joke_api # Blueprint import api definition
 from api.user import user_api # Blueprint import api definition
 from api.player import player_api
-from api.pokemon import pokemon_api
 
 
 # setup App pages
@@ -32,7 +30,6 @@ app.register_blueprint(covid_api) # register api routes
 app.register_blueprint(user_api) # register api routes
 app.register_blueprint(player_api)
 app.register_blueprint(app_projects) # register app pages
-app.register_blueprint(pokemon_api)
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
@@ -52,8 +49,6 @@ def activate_job():  # activate these items
     initJokes()
     initUsers()
     initPlayers()
-    initPokemons()
-
 # this runs the application on the development server
 if __name__ == "__main__":
     # change name for testing

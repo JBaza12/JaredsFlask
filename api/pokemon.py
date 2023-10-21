4d55094caac74fcb29607b95af6f9619f6db1ba5
RonitT1234 <ronit.joe@gmail.com>
2023-10-21 12:01:05 -0700

    Added and Complted Pokemon API, index out of range issue still needs to be fixed

---
+++ api/pokemon.py
@@ -0,0 +1,85 @@
+from flask import Blueprint, jsonify  # jsonify creates an endpoint response object
+from flask_restful import Api, Resource # used for REST API building
+import requests  # used for testing 
+import random
+
+from model.pokemons import *
+
+pokemon_api =  Blueprint('pokemon_api', __name__,
+                   url_prefix='/api/pokemons')
+
+api = Api(pokemon_api)
+
+class PokemonsAPI:
+    
+    class _Create(Resource):
+        def post(self, pokemon):
+            pass
+        
+    class _Read(Resource):
+        def get(self):
+            return jsonify(getPokemons())
+        
+    class _ReadID(Resource):
+        def get(self, id):
+            return jsonify(getPokemon(id))
+    
+    class _ReadRandom(Resource):
+        def get(self):
+            return jsonify(getRandomPokemon())
+        
+    class _ReadCount(Resource):
+        def get(self):
+            count = countPokemons()
+            countMsg = {'count': count}
+            return jsonify(countMsg)
+    
+    class _UpdateUpVote(Resource):
+        def put(self, id):
+            addUpVote(id)
+            return jsonify(getPokemon(id))
+    
+    class _UpdateDownVote(Resource):
+        def put(self, id):
+            addDownVote(id)
+            return jsonify(getPokemon(id))  
+        
+    api.add_resource(_Create, '/create/<string:pokemon>')
+    api.add_resource(_Read, '/')
+    api.add_resource(_ReadID, '/<int:id>')
+    api.add_resource(_ReadRandom, '/random')
+    api.add_resource(_ReadCount, '/count')
+    api.add_resource(_UpdateUpVote, '/upvote/<int:id>')
+    api.add_resource(_UpdateDownVote, '/downvote/<int:id>') 
+    
+if __name__ == "__main__": 
+    
+    server = 'https://flask.nighthawkcodingsociety.com' # run from web
+    url = server + "/api/pokemons"
+    responses = [] 
+    
+    count_response = requests.get(url+"/count")
+    count_json = count_response.json()
+    count = count_json['count']
+    
+    num = str(random.randint(0, count-1)) # test a random record
+    responses.append(
+        requests.get(url+"/"+num)  # read joke by id
+        ) 
+    responses.append(
+        requests.put(url+"/upvote/"+num) # add to upvote count
+        ) 
+    responses.append(
+        requests.put(url+"/downvote/"+num) # add to downvote count
+        ) 
+    
+    responses.append(
+        requests.get(url+"/random")  # read a random joke
+        ) 
+    
+    for response in responses:
+        print(response)
+        try:
+            print(response.json())
+        except:
+            print("unknown error")
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from __init__ import app,db  # Definitions initialization
 from model.jokes import initJokes
 from model.users import initUsers
 from model.players import initPlayers
+from model.pokemons import initPokemons
 
 
 # setup APIs
@@ -15,6 +16,7 @@ from api.covid import covid_api # Blueprint import api definition
 from api.joke import joke_api # Blueprint import api definition
 from api.user import user_api # Blueprint import api definition
 from api.player import player_api
+from api.pokemon import pokemon_api
 
 
 # setup App pages
@@ -30,6 +32,7 @@ app.register_blueprint(covid_api) # register api routes
 app.register_blueprint(user_api) # register api routes
 app.register_blueprint(player_api)
 app.register_blueprint(app_projects) # register app pages
+app.register_blueprint(pokemon_api)
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
